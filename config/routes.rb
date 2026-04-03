5df766a0249901bb2d33f7b4bc41090053ec796a
MissakaLap <missakabro@gmail.com>
2026-04-03 09:21:57 +0000

    Finished the latest updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :ingredients
+  resources :recipes
+  resources :users
+  #get 'about/index'
+  #get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "home#index"
+  get '/about', to: "about#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
